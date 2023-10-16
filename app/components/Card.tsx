b605569a20757bcdd74e28a161acd66bf9d55577
hyper-neutrino <alex.yj.liao@gmail.com>
2023-10-16 10:04:11 -0400

    fix alignment for cards on narrow screens

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -6,9 +6,8 @@ interface IProps extends React.PropsWithChildren {
 const Card: React.FC<IProps> = ({ children, title, wide }) => {
     return (
         <div
-            className={`relative bg-gray-700/40 hover:bg-gray-600/40 rounded-xl p-10 flex flex-col justify-between gap-6 backdrop-blur-sm transition duration-250 ${
-                wide ? "col-span-2" : ""
-            }`}
+            className="relative bg-gray-700/40 hover:bg-gray-600/40 rounded-xl p-10 flex flex-col justify-between gap-6 backdrop-blur-sm transition duration-250"
+            style={{ gridColumn: wide ? "1 / -1" : "" }}
         >
             <div className="flex flex-col gap-6">
                 <b className="text-4xl text-teal/80">{title}</b>
