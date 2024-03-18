bc4184be0ae9b8215bded0d7bda0f68fadaf18ea
hyper-neutrino <alex.yj.liao@gmail.com>
2024-03-18 03:51:17 -0400

    shorten wave 26 supplement

--- app/wave-26/page.tsx
+++ app/wave-26/page.tsx
@@ -11,10 +11,8 @@ export default function Wave26() {
                     This page serves as additional information for HyperNeutrino&prime;s statement. Please read the statement itself in the election thread.
                 </p>
                 <p className="text-lg">
-                    I skipped this reflection last election in favor of deformalizing communications to establish a more human tone and address the perception
-                    of observers as an indifferent unit or organization, but for this election, I would like to bring this back as I believe it&prime;s a
-                    valuable reflection, both for myself to look back on my effectiveness over the last term and for voters to evaluate whether they really want
-                    me to continue as an observer.
+                    I skipped this reflection last election, but for this election, I would like to bring this back as I believe it&prime;s a valuable
+                    reflection, both for myself and to give voters the ability to make a more informed decision.
                 </p>
                 <hr />
                 <h1 className="text-4xl">
@@ -22,27 +20,15 @@ export default function Wave26() {
                 </h1>
                 <p className="text-lg">Last term, my priorities were to focus on two key improvements:</p>
                 <ol className="pl-8 list-decimal">
-                    <li>
-                        <b>Deformalize observer communications.</b> Specifically, I stated that the way in which statements used to be conducted was tiring for
-                        myself and made communication with the observer team uncomfortable, so I would try to make talking to observers feel more like speaking
-                        to humans instead of a corporate entity.
-                    </li>
-                    <li>
-                        <b>Be more direct and isolate what is important to address.</b> Tying into the first point as well, previously I engineered my wording
-                        pretty cautiously to avoid drama, which in turn made things sometimes confusing or further appear corporate. I stated that I would work
-                        on communicating more openly and directly (which additionally would hopefully incentivize others to do the same) and filter things that
-                        weren&prime;t really important from statements, cutting context in favor of readability.
-                    </li>
+                    <li>Deformalize observer communications.</li>
+                    <li> Be more direct and isolate what is important to address.</li>
                 </ol>
                 <p className="text-lg">
-                    Since last election, few official statements have been given. Of them, none of them really contributed to tensions as they were all
-                    technical or just information about upcoming events or disclosures.
+                    Since last election, few official statements have been given. Of them, they were mostly just technical or information.
                 </p>
                 <p className="text-lg">
                     Overall, I believe that the above two points have been accomplished this term, though there have been fewer opportunities to demonstrate
-                    change compared to previous terms. For example, I have been communicating more as just myself without feedback from the team (which has been
-                    explicitly disclosed), seeking to speak less formally, and although I have not surveyed anyone regarding this matter, it seems that
-                    communication with observers is more open now and we have not received more feedback about difficulty communicating with us.
+                    change compared to previous terms.
                 </p>
                 <hr />
                 <h1 className="text-4xl">
@@ -54,48 +40,38 @@ export default function Wave26() {
                 </p>
                 <h2 className="text-2xl">Summary</h2>
                 <p className="text-lg">
-                    My summary from my Wave 19 statement stated that my priorities were (1) establishing an environment where everyone feels comfortable and
-                    safe communicating openly, (2) establishing clear and rigorous protocols, and (3) maintaining transparency. I believe that these have been
-                    accomplished &mdash; although we still struggle with some tensions in communication, that is unavoidable when stakes are high and in the
-                    interest of protecting open communication, we cannot stop people from engaging in what may not necessarily always be good faith.
+                    My summary from my Wave 19 statement stated that my priorities were establishing an environment where everyone feels safe communicating
+                    openly, establishing clear and rigorous protocols, and maintaining transparency. I believe that these have been accomplished; although there
+                    is still sometimes tension, that is unavoidable.
                 </p>
                 <p className="text-lg">
-                    Our protocols have continued to improve and I have pushed for and implemented many changes, both from my own proposals (such as formalizing
-                    the banshare policy to improve our quality control and overhauling the constitution to improve its rigor without sacrificing clarity) and by
-                    supporting others&prime; in changing things around the network (such as the tie-breaker proposal for induction votes and the formalization
-                    of the observation report feedback cycle).
+                    Our protocols have continued to improve and I have pushed for and implemented many changes, both from my own proposals and by supporting
+                    others&prime; in changing things around the network.
                 </p>
                 <p className="text-lg">
-                    Finally, I believe the observer team has continued to be sufficiently transparent. Comparatively fewer things have happened in the past year
-                    where this has been applicable, but we were transparent during incidents such as regarding internal team conflicts, the unexpected transfer
-                    of network assets to me from Leaf, etc.
+                    Finally, I believe the observer team has continued to be sufficiently transparent. Fewer things have happened in the past year requiring
+                    transparency, but when needed, the team has been upheld this.
                 </p>
                 <h2 className="text-2xl">Campaign Promises</h2>
                 <p className="text-lg">
-                    My campaign promises were to establish more rigorous rules and rewrite the TCN website to serve purposes such as better structured rules,
-                    accessible guides, and public information/FAQ pages. In previous terms, I worked to make the constitution more clear and accessible compared
-                    to its original form, which was often confusing due to its legal-like wording. In the past year, I revised the rules and guidelines back
-                    into the constitution, reflecting its purpose not as server rules but as a document for the entire network itself, establishing and
-                    reintroducing its rigor while maintaining readable wording and formatting.
+                    My campaign promises were to establish more rigorous rules and rewrite the TCN website. In previous terms, I worked to make the constitution
+                    more accessible. In the past year, I revised the rules and guidelines back into the constitution, better reflecting its purpose and
+                    reintroducing rigor without sacrificing clarity.
                 </p>
                 <p className="text-lg">
-                    Additionally, I completed a rewrite of the entire website and all other assets (including the global chat relay and voting system) following
-                    Leaf&prime;s departure, which comes with many technical and developer experience improvements, but importantly now hosts our new
-                    constitution and many other guidelines, onboarding guides for new servers and observers, and public FAQ pages for those looking to research
-                    the network.
+                    Additionally, I completed a rewrite of the entire system, which comes with many technical and developer experience improvements, but most
+                    importantly now hosts our new constitution and many other guides and public information pages.
                 </p>
                 <h2 className="text-2xl">What to Expect</h2>
                 <p className="text-lg">
-                    During my Wave 19 statement, I said that if I were re-elected, the council could expect me to seek to maintain the stable and calm position
-                    that the network was in at the time and make small improvements in several areas. The network has been less stable in the past year than at
-                    the time of this statement, but a large amount of that was caused by trying to maintain peace and control conflicts before they caused
-                    catastrophic damage, and I believe that the observer team was overall successful in minimizing destabilization.
+                    During my Wave 19 statement, I said that if I were re-elected, you could expect me to maintain the stable and calm position that the network
+                    was in at the time and make small improvements in several areas. The network has been less stable in the past year than at the time of this
+                    statement, but I believe that has mostly been due to external factors and that the team has done its best to control it when it happens.
                 </p>
                 <p className="text-lg">
                     I also promised to be a leader who would take ownership of controlling conflicts and take accountability for and implement improvements to
                     any individual, observer, or council mistakes. I believe that I have accepted responsibility wherever applicable for anything the observer
-                    team could have done better, and in the past year, I have established a postmortem process to ensure mistakes aren&prime;t made twice and
-                    taken personal accountability for improving observer-council relations.
+                    team could have done better.
                 </p>
                 <hr />
                 <h1 className="text-4xl">Concluding Words</h1>
@@ -109,9 +85,8 @@ export default function Wave26() {
                 </p>
                 <p className="text-lg">
                     This term, I will aim to implement a more solid feedback loop, as I believe the network is not currently struggling with any specific issues
-                    that need priority on fixing. As such, I will strive to look proactively for areas that may be improved even if they are not failing at the
-                    moment and seek to constantly revisit our policies and procedures and identify how we can make the network better for its servers and the
-                    environment better for the council.
+                    that need priority on fixing. As such, I will look proactively for areas that may be improved and revisit our policies and procedures and
+                    identify how we can make the network better for everyone.
                 </p>
                 <p className="text-lg">
                     If I am re-elected, you can expect that I will continue taking personal responsibility for upholding the network&prime;s values and ensuring
